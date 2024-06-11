a22f2d2b4847c179b512a5365a05ce29ec9c427e
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-11 23:51:06 +0900

    feat: Toast message 도입

--- components/home/searchResult.tsx
+++ components/home/searchResult.tsx
@@ -1,13 +1,19 @@
 import FlexBox from "../Flexbox";
 
-export default function SearchResult() {
+interface SearchResultProps {
+  onClick?: () => void;
+}
+
+export default function SearchResult({ onClick }: SearchResultProps) {
   return (
     <FlexBox className="w-full border-b p-2 justify-between">
       <FlexBox direction="col" className="gap-1 items-start">
         <div className="h3">풍물패 두드림</div>
         <div className="h4 text-gray-700">서울시립도서관 4층 | 월 16시</div>
       </FlexBox>
-      <div className="h2 text-main-color p-2">참여</div>
+      <div className="h2 text-main-color p-2" onClick={onClick}>
+        참여
+      </div>
     </FlexBox>
   );
 }
