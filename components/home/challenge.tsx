9e18d965527dfe1e0d45fd774470e4a7e6d3d914
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-13 19:42:06 +0900

    feat: 일반, 관리자 화면 분기

--- components/home/challenge.tsx
+++ components/home/challenge.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
 import FlexBox from "../Flexbox";
 import { useRouter } from "next/router";
+import { useAtom } from "jotai";
+import { isAdminAtom } from "@/utils/atom";
 
 export const Challenge: NextPage = () => {
   const router = useRouter();
+  const [isAdmin] = useAtom(isAdminAtom);
 
   return (
     <FlexBox direction="col" className="p-4 w-full rounded-lg border gap-4">
@@ -18,17 +21,19 @@ export const Challenge: NextPage = () => {
       </FlexBox>
       <FlexBox direction="col" className="w-full gap-1">
         <FlexBox className="w-full items-start justify-between">
-          <FlexBox className="gap-1">
-            <div className="h5 text-gray-500">개인달성률</div>
-            <div className="h3">100%</div>
-          </FlexBox>
+          {!isAdmin && (
+            <FlexBox className="gap-1">
+              <div className="h5 text-gray-500">개인달성률</div>
+              <div className="h3">100%</div>
+            </FlexBox>
+          )}
           <FlexBox className="gap-1">
             <div className="h5 text-gray-500">전체달성률</div>
             <div className="h3">100%</div>
           </FlexBox>
         </FlexBox>
         <div className="w-full border border-main-color rounded-lg py-2 text-center text-main-color h2">
-          인증하기
+          {isAdmin ? "현황 보러가기" : "인증하기"}
         </div>
       </FlexBox>
     </FlexBox>
--- components/home/challengeBox.tsx
+++ components/home/challengeBox.tsx
@@ -3,13 +3,18 @@ import FlexBox from "../Flexbox";
 import { Challenge } from "./challenge";
 import { HomeCarousel } from "./carousel";
 import { useRouter } from "next/router";
+import { isAdminAtom } from "@/utils/atom";
+import { useAtom } from "jotai";
 
 export const HomeChallenge: NextPage = () => {
   const router = useRouter();
+  const [isAdmin] = useAtom(isAdminAtom);
 
   return (
     <FlexBox direction="col" className="w-full items-start gap-2 p-4">
-      <div className="h1">참여 중인 챌린지</div>
+      <div className="h1">
+        {isAdmin ? "우리 센터에서 진행 중인 챌린지" : "참여 중인 챌린지"}
+      </div>
       <HomeCarousel />
       <Challenge />
       <Challenge />
@@ -17,7 +22,7 @@ export const HomeChallenge: NextPage = () => {
         className="w-full text-center bg-main-100 rounded-lg py-2 h2 text-gray-700"
         onClick={() => router.push("/challenge/join")}
       >
-        참여 프로그램 추가
+        {isAdmin ? "새 프로그램 등록" : "참여 프로그램 추가"}
       </div>
     </FlexBox>
   );
