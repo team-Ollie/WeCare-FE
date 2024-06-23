1ba426697a447e13dbb7b015e8fd99d4664dd46f
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-24 00:41:47 +0900

    feat: 토큰 set, get 설정

--- apis/client.ts
+++ apis/client.ts
@@ -1,8 +1,47 @@
 import axios from "axios";
 
+interface ResponseBody {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+}
+
+const setTokenFromLocalStorage = (access_token: string) => {
+  localStorage.setItem("access_token", access_token);
+};
+
+const getTokenFromLocalStorage = () => {
+  const accessToken = localStorage.getItem("access_token");
+  if (!accessToken) {
+    return null;
+  }
+  return accessToken;
+};
+
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Credentials": "true",
+  },
+  validateStatus: (status) => {
+    return status < 300;
+  },
 });
 
+client.interceptors.request.use(
+  async (config) => {
+    if (typeof document !== "undefined") {
+      const token = getTokenFromLocalStorage();
+      config.headers.set("Authorization", `Bearer ${token}`);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default client;
+export type { ResponseBody };
