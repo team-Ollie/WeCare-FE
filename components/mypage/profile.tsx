c3ad3f745beb4c2de36587417f12247e21442787
iOdiO89 <117376841+iOdiO89@users.noreply.github.com>
2024-06-27 02:15:59 +0900

    fix: 사용자 레벨에 따라 뱃지 다르게 출력

--- components/mypage/profile.tsx
+++ components/mypage/profile.tsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 import FlexBox from "../Flexbox";
 import { useRouter } from "next/router";
 import RightArrowIcon from "@/public/svgs/RightArrow.svg";
+import Level1Icon from "@/public/svgs/badges/1.svg";
+import Level2Icon from "@/public/svgs/badges/2.svg";
+import Level3Icon from "@/public/svgs/badges/3.svg";
+import Level4Icon from "@/public/svgs/badges/4.svg";
+import Level5Icon from "@/public/svgs/badges/5.svg";
+import Level6Icon from "@/public/svgs/badges/6.svg";
 
 interface ProfileProps {
   nickname: string;
@@ -10,6 +16,30 @@ interface ProfileProps {
 
 export default function Profile({ nickname, level }: ProfileProps) {
   const router = useRouter();
+
+  const returnBadge = () => {
+    switch (level) {
+      case 1:
+        return <Level1Icon width={24} height={24} />;
+        break;
+      case 2:
+        return <Level2Icon width={24} height={24} />;
+        break;
+      case 3:
+        return <Level3Icon width={24} height={24} />;
+        break;
+      case 4:
+        return <Level4Icon width={24} height={24} />;
+        break;
+      case 5:
+        return <Level5Icon width={24} height={24} />;
+        break;
+      case 6:
+      default:
+        return <Level6Icon width={24} height={24} />;
+        break;
+    }
+  };
   return (
     <FlexBox
       className="w-full py-7 px-6 justify-between"
@@ -18,7 +48,7 @@ export default function Profile({ nickname, level }: ProfileProps) {
       <FlexBox className="gap-2">
         <div className="w-10 h-10 rounded-full bg-gray-200" />
         <div className="h2">{nickname}</div>
-        <Image src={"/svgs/badges/3.svg"} width={24} height={24} />
+        {returnBadge()}
       </FlexBox>
       <RightArrowIcon width={20} height={20} />
     </FlexBox>
